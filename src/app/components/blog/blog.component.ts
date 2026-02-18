d9171e16d98cc4ed783a6ff7a75037ba07050c02
Miguel <miguelozonas@gmail.com>
2026-02-18 17:45:30 +0100

    cambios finales

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -10,6 +10,7 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './blog.component.css',
 })
 export class BlogComponent {
+  
   arrayNews: INew[] = news.sort((a, b) => 
     new Date(b.fecha).getTime() - new Date(a.fecha).getTime()
   );
@@ -18,19 +19,39 @@ export class BlogComponent {
     titulo: '',
     descripcion: '',
     imagen: '',
-    fecha: new Date()
+    fecha : new Date()
   };
+  
+  imageUnfound: string='https://images.squarespace-cdn.com/content/v1/552966fce4b0bad8e3089670/1428798390946-OZ6K57K6I9ASYD028WZ3/UC+-+Company+Logo.jpg?format=1500w'
 
   guardarNoticia() {
-    if (this.news.titulo && this.news.descripcion && this.news.imagen && this.news.fecha) {
-      this.arrayNews.unshift({ ...this.news });
-      this.news = {
-        titulo: '',
-        descripcion: '',
-        imagen: '',
-        fecha: new Date()
-      };
-    }
+  const errores: string[] = [];
+
+  if (!this.news.titulo) {
+    errores.push('- Título');
   }
+  if (!this.news.descripcion) {
+    errores.push('- Descripción');
+  }
+  if (!this.news.imagen) {
+    errores.push('- URL de la imagen');
+  }
+  if (!this.news.fecha) {
+    errores.push('- Fecha');
+  }
+
+  if (errores.length > 0) {
+    alert('Campos obligatorios vacíos:\n' + errores.join('\n'));
+    return;
+  }
+
+  this.arrayNews.unshift({ ...this.news });
+  this.news = {
+    titulo: '',
+    descripcion: '',
+    imagen: '',
+    fecha: new Date()
+  };
+}
 
 }
--- src/app/db/news.db.ts
+++ src/app/db/news.db.ts
@@ -2,39 +2,27 @@ import { INew } from "../models/inew";
 
 export const news: INew[] = [
     {
-        titulo: 'Noticia 1',
-        descripcion: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        fecha: new Date('2024-06-01'),
-        imagen: 'https://via.placeholder.com/150'
-    },
-    {
-        titulo: 'Noticia 2',
-        descripcion: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        fecha: new Date('2024-06-02'),
-        imagen: 'https://dummyimage.com/300'
-    },
-    {
-        titulo: 'Noticia 3',
-        descripcion: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        titulo: 'Max Cabanes',
+        descripcion: 'Max Cabanes, nacido el 22 de septiembre de 1947 en Béziers, es un ilustrador y autor de cómics francés, conocido principalmente por su serie de fantasía "Dans les villages" (publicada desde 1977) y por sus cómics que retratan la vida de los jóvenes en las décadas de 1960 y 1970 (Colin Maillard, "Les Années pattes d eph", "Ramo de flirts"). Dominado por todas las técnicas de dibujo y capaz de destacar en una amplia gama de géneros (fantasía, crítica social, novela negra), este "extremadamente talentoso creador de mundos[1]" recibió el Gran Premio de la Ciudad de Angoulême en 1990, el máximo galardón para un autor de cómics en francés.',
         fecha: new Date('2024-06-03'),
-        imagen: 'https://dummyimage.com/300'
+        imagen: 'https://www.bdangouleme.com/medias/selection/2024/bandeau_phX1/SP-04-206-p2.jpg'
     },
     {
-        titulo: 'Noticia 4',
-        descripcion: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        titulo: 'Charles Vess',
+        descripcion: 'Charles Vess (nacido el 10 de junio de 1951)[1] es un artista estadounidense de fantasía y cómic especializado en la ilustración de mitos y cuentos de hadas. Entre sus influencias se encuentran el ilustrador británico de la Edad de Oro Arthur Rackham, el pintor checo de estilo Art Nouveau Alphonse Mucha y el dibujante de cómics Hal Foster, entre otros. Vess ha ganado varios premios por sus ilustraciones. Su estudio, Green Man Press, se encuentra en Abingdon, Virginia.Ha recibido numerosos premios y distinciones por su trabajo, incluyendo el Premio Locus al Mejor Artista de 2019 y 2023, y los Premios Hugo de 2019 al Mejor Artista Profesional y al Mejor Libro de Arte por The Books of Earthsea: The Complete Illustrated Edition.[2] En 1991, su trabajo con Neil Gaiman en el relato corto de Sandman "Sueño de una noche de verano" se convirtió en el primer cómic en ganar el Premio Mundial de Fantasía.[3]',
         fecha: new Date('2024-06-04'),
-        imagen: 'https://dummyimage.com/300'
+        imagen: 'https://arthive.com/res/media/img/orig/work/03f/118571.jpg'
     },
     {
-        titulo: 'Noticia 5',
-        descripcion: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        titulo: 'Moebius',
+        descripcion: 'Jean Giraud (Nogent-sur-Marne, Val-de-Marne; 8 de mayo de 1938-París, 10 de marzo de 2012)[1]​ fue un historietista, ilustrador y escritor francés, que trabajó en la tradición de historietas franco-belgas. Giraud ganó reconocimiento mundial bajo el seudónimo de Mœbius, así como el de Gir, que usó para la serie wéstern El Teniente Blueberry (1964) y para sus pinturas con temáticas wéstern. Admirado por Federico Fellini, Stan Lee y Hayao Miyazaki, entre otros,[2]​ ha sido descrito como el artista de bande dessinée más influyente por detrás de Hergé.[3]​ En 2014, la revista especializada Comic Book Resources lo situó en el puesto 11 de su lista de los mejores artistas de cómic de todos los tiempos.[4]​POR CIERTO, SE AVECINA PELICULA SOBRE EL INCAL',
         fecha: new Date('2024-06-05'),
-        imagen: 'https://dummyimage.com/300'
+        imagen: 'https://i.pinimg.com/736x/b0/4e/4e/b04e4e5e291b22778c2db3f5e1935eea.jpg'
     },
     {
-        titulo: 'Noticia 6',
-        descripcion: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        titulo: 'Richard Corben',
+        descripcion: 'Richard Vance Corben (Anderson, Misuri, 1 de octubre de 1940 - 2 de diciembre de 2020),[2]​ conocido como Richard Corben o con los seudónimos Gore, Corb y Harvey Sea, fue un historietista estadounidense, célebre por sus cómics de ciencia ficción y fantasía de abigarrado e innovador colorido, cuyos fotolitos creaba y procesaba personalmente. Procedente del underground, adquirió condición de autor de culto a finales de los años setenta,[3]​ acercándose al cómic para adultos de la Warren Publishing y a la incipiente novela gráfica de su época.[4]​ En 2012, la revista especializada Comic Book Resources lo situó en el puesto 10 de su lista de los mejores artistas de cómic de todos los tiempos.[5]​',
         fecha: new Date('2024-06-06'),
-        imagen: 'https://dummyimage.com/300'
+        imagen: 'https://cdnb.artstation.com/p/assets/images/images/045/616/797/large/richard-corben-museum-1978-pilgor-discusses-politics-with-his-friends-copia.jpg?1643140536'
     }
 ];                      
