1102a5a983c9528ba44422f0aaa30078576ec0e1
Miguel <miguelozonas@gmail.com>
2026-02-18 15:52:46 +0100

    funcionamiento del formulario
    queda pendiente arreglar temas de tamaño de imágenes y alguna cosilla más

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { news } from '../../db/news.db';
 import { INew } from '../../models/inew';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-blog',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css',
 })
@@ -12,4 +13,24 @@ export class BlogComponent {
   arrayNews: INew[] = news.sort((a, b) => 
     new Date(b.fecha).getTime() - new Date(a.fecha).getTime()
   );
+  
+  news: INew = {
+    titulo: '',
+    descripcion: '',
+    imagen: '',
+    fecha: new Date()
+  };
+
+  guardarNoticia() {
+    if (this.news.titulo && this.news.descripcion && this.news.imagen && this.news.fecha) {
+      this.arrayNews.unshift({ ...this.news });
+      this.news = {
+        titulo: '',
+        descripcion: '',
+        imagen: '',
+        fecha: new Date()
+      };
+    }
+  }
+
 }
